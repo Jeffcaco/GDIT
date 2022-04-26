ddbcc178cf89069de669d44bce7a788d9493a6e2
Jeffcaco <jeffreycaco98@gmail.com>
2022-04-25 20:58:43 -0500

    actualizacion

--- Login/index.php
+++ Login/index.php
@@ -5,11 +5,12 @@
          header("Location:../Principal/");
      }else{
             //SI NO EXISTE. VERIFICAR SI SE HIZO UN POST
-            if(isset($_POST['user']) && isset($_POST['password'])){
+            if(isset($_POST['user']) && isset($_POST['password']) && isset($_POST['tipo'])){
               session_start();
                 $user = $_POST['user'];
                 $password = $_POST['password'];
-                $query = "SELECT * FROM administrador WHERE correo = '$user' AND password = '$password'";
+                $tipo = $_POST['tipo'];
+                $query = "SELECT * FROM administrador WHERE correo = '$user' AND password = '$password' AND tipo = '$tipo'";
                 //die($query);
                 include_once("../Database/conexion.php");
                 $result = mysqli_query($conexion, $query);
@@ -17,9 +18,15 @@
                     $row = mysqli_fetch_array($result);
                     $_SESSION['user'] = $row['correo'];
                     $_SESSION['contra'] = $row['password'];
+                    $_SESSION['tipo'] = $row['tipo'];
                     //cerrar la conexion a la base de datos a la vez que se cierra el script
                     mysqli_close($conexion);
-                    header("Location:../Principal/");
+                    if($tipo=="admi"){
+                        header("Location:../Principal2/");
+                    }else{
+                        header("Location:../Principal/");
+                    }
+                    
                 }else{
    
                   echo '
@@ -36,82 +43,63 @@
 
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>GDIT | Logística</title>
-  <!--Developers 
-        @Castillo Cornejo, Jeffrey Bryan		
-        @Mitma Huaccha, Johan Valerio  	-->
+    <!--************************************************************
+     *   Developer                                                 *
+     *   @Castillo Cornejo, Jeffrey Bryan                          *
+    *************************************************************-->
+    <title>GDIT | Gerencia Logística</title>
     <!-- Required meta tags -->
-	<script src="https://kit.fontawesome.com/31127b7562.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="estilos.css">
-  <link rel="icon" type="image" href="../gdit.jfif">
-	<meta charset="utf-8">
-  <!--Bootstrap-->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/31127b7562.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="login-form-6.css">
+    <link rel="icon" type="image" href="../gdit.jfif">
+    <meta charset="utf-8">
+    <!--Bootstrap-->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="http://infiniteiotdevices.com/images/logo.png" rel="icon" sizes="16x16" type="image/gif" />
+
+    <!--Bootstrap-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  
-	<style>
-		body{
-			background-image: url('../gdit.jfif');
-			object-fit:cover; 
-			background-repeat: no-repeat;
-      background-position:center 50%;
-		}
-	</style>
 
 </head>
-<body class="container" style="margin-bottom:0px; padding-bottom:0px;">
-	
-	<header>
-      <div class="b">GDIT - Grupo de difusión e innovación tecnológica</div>
-      <div class="a">Logística | Administración de datos</div>
-      
-	</header>
 
-	<main>
-      <div class="titulo">
-        INICIAR SESION  
-      </div>
-      <div class="login">
-        <form action="index.php" method="post" style="margin-top:5%;">
-          <div class="icono"><i class="fas fa-user-circle fa-6x"></i></div>
-          <div>
-              <div style="margin-left:10%; margin-bottom:15px;">
-                      <label for="user" style="color:white;">Usuario</label>
-                      <input type="text" class="form-control" name="user" aria-describedby="emailHelpId" placeholder="Ingrese su correo" required style="width:85%; text-align:center; font-size:18px;">
-                                      
-              </div>
-              
-              <div style="margin-left:10%;">
-                      <label for="password" style="color:white;">Contraseña</label>
-                      <input type="password" class="form-control" name="password" placeholder="Ingrese su contraseña" minlength="8" required style="width:85%; text-align:center; font-size:18px;">
-              </div>
-              <br>
-              <div style="text-align:center;">
-                  <button type="submit" class="btn btn-dark" style="font-size:20px;">INGRESAR</button>
-              </div>
-          </div>
-        </form>
-		</div>
-    
-    <div class="auxiliar">
-      <br><br>
-    </div>
+<body>
+<div class="form">
+		<form action="index.php" method="post" >
+			<h2>INICIAR SESION</h2>
+			<div class="input-box">
+				<i class="fa fa-user"></i>
+				<input type="text" name="user" placeholder="Correo" required="true">
+			</div>
+			<div class="input-box">
+				<i class="fa fa-lock"></i>
+				<input type="password" name="password" placeholder="Password" required="true">
+			</div>
+			<div class="form-group">
+                
+                <select class="form-control" name="tipo">
+                    <option value="">-- Seleccionar tipo --</option>
+                    <option value="admi">Administrador</option>
+                    <option value="usu">Usuario</option> 
+                </select> 
+            </div>
+            <div class="input-box" style="text-align: center;">
+				<input type="submit" name="" value="INGRESAR">
+			</div>
+			<hr>
+            <p style="text-align: center;">© Todos los derechos reservados 
+                <?php
+                     echo date("Y");
+                ?></p>
+            
+		</form>
+	</div>
 
-	</main>
-
-	<footer>
-		<div class="pie">
-			
-      <p>© Todos los derechos reservados 
-        <?php
-          echo date("Y");
-        ?></p>
-		</div>
-	</footer>
-	
 </body>
+
 </html>
